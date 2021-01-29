cf8ae93faac5379b12a72b29f380dde48e91125e
dharankumar2431 <dharankumar2431@gmail.com>
2021-01-29 10:45:07 +0530

    sample

---
+++ javascript/example.js
@@ -0,0 +1,95 @@
+// var d;
+// var b;
+let a;
+function exampleButton(){
+    let el = document.getElementById('click');
+    el.innerHTML = '<p>welcome to javascript to learn</p>';
+    el.style = 'font-size:50px';
+    document.getElementById('button').style = 'background-color:blue;color:white';
+}
+
+function add(){
+  var d = 10;
+if(true){
+     a = 30;
+     var b = 20;
+    console.log(d + b + a);
+}
+d = 80;
+console.log(d + b);
+console.log(d + b + a);
+}
+
+function massage(){
+    window.alert('this is js');
+}
+
+var x = 50;
+var y = 60;
+var z = x + y;
+var w = x - y;
+var e = x * y;
+var r = x ** y;
+var t = x / y;
+var i = ++x;
+var u = --y;
+x = y;
+
+
+console.log(z);
+console.log(w);
+console.log(e);
+console.log(r);
+console.log(t);
+console.log(i);
+console.log(u);
+console.log(x);
+x += y;
+console.log(x);
+x -= y;
+console.log(x);
+x *= y;
+console.log(x);
+x **= y;
+console.log(x);
+x /= y;
+console.log(x);
+
+
+s = x == y;
+console.log(s);
+
+f = x===y;
+console.log(f);
+
+h = x != y;
+console.log(h);
+
+j = x !== y;
+console.log(j);
+
+k = x > y;
+console.log(k);
+
+l = x < y;
+console.log(l);
+
+c = x >= y;
+console.log(c);
+
+n = x <= y;
+console.log(n);
+
+// p = x ? y 
+// console.log(p);
+
+o = x & y;
+console.log(o);
+
+
+
+
+
+
+
+
